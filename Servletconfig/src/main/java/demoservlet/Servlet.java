3cdd209e3972144386dc40a6d580f45276899b07
Vinitha-02 <104415276+Vinitha-02@users.noreply.github.com>
2022-05-24 13:13:58 +0530

    Add files via upload

---
+++ Servletconfig/src/main/java/demoservlet/Servlet.java
@@ -0,0 +1,27 @@
+package demoservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class Servlet extends HttpServlet {  
+public void doGet(HttpServletRequest request, HttpServletResponse response)  
+    throws ServletException, IOException {  
+  
+    response.setContentType("text/html");  
+    PrintWriter out = response.getWriter();  
+      
+    ServletConfig config=getServletConfig();  
+    String driver=config.getInitParameter("driver");  
+    out.print("Driver is: "+driver);  
+          
+    out.close();  
+    }  
+  
+}  
